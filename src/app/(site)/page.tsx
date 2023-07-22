fa0dd4a2c91cde1b710d3d530476855962b22372
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-07-22 19:50:14 +0700

    Update page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,13 @@
+"use client"
+import { trpc } from "@/utils/trpc";
 import Link from "next/link";
 
 export default function Home() {
+  let { data: users, isLoading, isFetching } = trpc.coba.coba.useQuery();
+
+  if (isLoading || isFetching) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="relative flex flex-col justify-center items-center w-full min-h-screen bg-primaryYellow">
@@ -20,6 +27,14 @@ export default function Home() {
             <p className="pt-2 text-sm text-primaryBg font-medium opacity-[90%] hover:opacity-[100%] transition-all">
               {"Don’t have an account? Sign Up"}
             </p>
+            {
+              users && 
+              <p>
+                {
+                  users.name
+                }
+              </p>
+            }
           </Link>
         </div>
       </div>
