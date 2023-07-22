b692683d3b7bb3abe10bf3a66851ff8ca16e8340
farhan <mohdfarhanfahrezy@gmail.com>
2023-07-22 23:28:41 +0700

    fix : add text to navbar

--- src/app/components/MobileNavbar.tsx
+++ src/app/components/MobileNavbar.tsx
@@ -16,25 +16,27 @@ const MobileNavbar = () => {
     <div className="fixed bottom-0 flex flex-row w-full bg-primaryBg justify-around items-center py-3">
       <Link
         href={"/home"}
-        className="hover:scale-[1.05] active:scale-[0.95] transition-all"
+        className="flex flex-col justify-center items-center hover:scale-[1.05] active:scale-[0.95] transition-all"
       >
         <AiFillHome
-          size={35}
+          size={30}
           color={pathname === "/home" ? "#FF914D" : "#3E3C3C"}
         />
+        <div className="text-[12px] pt-2">Home</div>
       </Link>
       <Link
         href={"/courses"}
-        className="hover:scale-[1.05] active:scale-[0.95] transition-all"
+        className="flex flex-col justify-center items-center hover:scale-[1.05] active:scale-[0.95] transition-all"
       >
         <FaBookBookmark
           size={30}
           color={pathname === "/courses" ? "#FF914D" : "#3E3C3C"}
         />
+        <div className="text-[12px] pt-2">Course</div>
       </Link>
       <Link
         href={"/daily-task"}
-        className="hover:scale-[1.05] active:scale-[0.95] transition-all"
+        className="flex flex-col justify-center items-center first-line:hover:scale-[1.05] active:scale-[0.95] transition-all"
       >
         <Image
           src={"/default-profile.jpg"}
@@ -45,26 +47,29 @@ const MobileNavbar = () => {
           loading="lazy"
           className={`${
             pathname === "/daily-task" && "border-2 border-[#FF914D]"
-          } w-[50px] aspect-square rounded-full`}
+          } w-[40px] aspect-square rounded-full`}
         />
+        <div className="text-[12px] pt-2">Daily Task</div>
       </Link>
       <Link
         href={"/ai-assistant"}
-        className="hover:scale-[1.05] active:scale-[0.95] transition-all"
+        className="flex flex-col justify-center items-center hover:scale-[1.05] active:scale-[0.95] transition-all"
       >
         <SiGoogleassistant
           size={30}
           color={pathname === "/ai-assistant" ? "#FF914D" : "#3E3C3C"}
         />
+        <div className="text-[12px] pt-2">Assist</div>
       </Link>
       <Link
         href={"/settings"}
-        className="hover:scale-[1.05] active:scale-[0.95] transition-all"
+        className="flex flex-col justify-center items-center hover:scale-[1.05] active:scale-[0.95] transition-all"
       >
         <IoSettings
           size={30}
           color={pathname === "/settings" ? "#FF914D" : "#3E3C3C"}
         />
+        <div className="text-[12px] pt-2">Settings</div>
       </Link>
     </div>
   );
