dfbc08f569fdd633ba046e0d0663fc2df15fa031
farhan <mohdfarhanfahrezy@gmail.com>
2023-07-22 18:06:42 +0700

    fix: fix conflict

--- src/app/(site)/register/page.tsx
+++ src/app/(site)/register/page.tsx
@@ -9,8 +9,7 @@ export default function Home() {
   // CONST
   const [data, setData] = useState({
     name: "",
-    username: "",
-    birthday: new Date(),
+    email: "",
     password: "",
     confirmPassword: "",
   });
@@ -28,7 +27,7 @@ export default function Home() {
     <div className="relative flex flex-col justify-center items-center w-full min-h-screen bg-primaryBg">
       <form
         onSubmit={submitRegistration}
-        className="absolute bottom-[5vh] flex flex-col w-full justify-center items-center gap-[5vh]"
+        className="absolute bottom-[5vh] flex flex-col w-full justify-center items-center gap-[15vh]"
       >
         <div className="w-full max-w-[350px] space-y-5">
           <div className="w-full text-center font-bold text-[24px]">
@@ -59,44 +58,22 @@ export default function Home() {
           </div>
           <div>
             <label
-              htmlFor="username"
+              htmlFor="email"
               className="block text-md font-medium leading-6"
             >
-              Username
+              Email
             </label>
             <div className="mt-2">
               <input
-                id="username"
-                name="username"
-                type="text"
-                autoComplete="text"
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
                 required
-                placeholder="notspidey"
-                value={data.username}
+                placeholder="example@mail.com"
+                value={data.email}
                 onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                  setData({ ...data, username: e.target.value });
-                }}
-                className="block w-full rounded-md border-0 py-1.5 shadow-sm text-black placeholder:text-secondayGray placeholder:opacity-[50%] sm:text-sm sm:leading-6 focus:px-2 transition-all"
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor="dateofbirth"
-              className="block text-md font-medium leading-6"
-            >
-              Date of birth
-            </label>
-            <div className="mt-2">
-              <input
-                id="dateofbirth"
-                name="dateofbirth"
-                type="date"
-                autoComplete="date"
-                required
-                value={data.birthday.toISOString().split("T")[0]}
-                onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                  setData({ ...data, birthday: new Date(e.target.value) });
+                  setData({ ...data, email: e.target.value });
                 }}
                 className="block w-full rounded-md border-0 py-1.5 shadow-sm text-black placeholder:text-secondayGray placeholder:opacity-[50%] sm:text-sm sm:leading-6 focus:px-2 transition-all"
               />
