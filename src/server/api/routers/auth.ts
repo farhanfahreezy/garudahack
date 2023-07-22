c12f8dfb54fa941b3a90b69ab233fef90bd87eab
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-07-22 18:32:49 +0700

    Update auth.ts

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { generateHash } from "@/utils/auth";
+import { compareHash, generateHash } from "@/utils/auth";
 
 export const authRouter = createTRPCRouter({
   resetPassword: protectedProcedure
     .input(
       z.object({
         email: z.string().email(),
-        password: z.string().min(8),
+        newPassword: z.string().min(8),
+        oldPassword: z.string().min(8),
       })
     )
     .query(async ({ ctx, input }) => {
@@ -17,6 +18,7 @@ export const authRouter = createTRPCRouter({
         },
         select: {
           id: true,
+           password: true,
         },
       });
       if (!profile) {
@@ -25,12 +27,20 @@ export const authRouter = createTRPCRouter({
           message: "User not found",
         };
       }
+
+      const isPasswordValid = await compareHash(input.oldPassword, profile.password);
+      if(!isPasswordValid){
+        return {
+            success: false,
+            message: "Old password is wrong",
+            };
+        }
       await ctx.prisma.user.update({
         where: {
             id: profile.id,
         },
         data: {
-            password: await generateHash(input.password)
+            password: await generateHash(input.newPassword)
         }
         })
 
